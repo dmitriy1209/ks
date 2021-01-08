cc50f35e969e7a26968f5d1a0dc099b4de1a2dd7
Dmitriy_PC\Dmitriy <prizrak.opery.13@gmail.com>
2021-01-09 01:46:36 +0200

    fix project

--- TableView/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.tableview {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.tableview to javafx.fxml;
-    exports com.mycompany.tableview;
-}
